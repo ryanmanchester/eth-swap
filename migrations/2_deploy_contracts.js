32cb7269bc1012b36252f741bd7f714b4307a8db
Ryan Manchester <ryanm17@gmail.com>
2022-01-01 18:43:21 -0500

    creates token and transfers total balance to marketplace

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,10 @@
+const Token = artifacts.require("Token");
 const EthSwap = artifacts.require("EthSwap");
 
-module.exports = function(deployer) {
-  deployer.deploy(EthSwap);
+module.exports = async function(deployer) {
+  await deployer.deploy(Token); //deploy token
+  const token = await Token.deployed();
+  await deployer.deploy(EthSwap); //deploy eth swap platform
+  const ethSwap = await EthSwap.deployed();
+  await token.transfer(ethSwap.address, '1000000000000000000000000');
 };

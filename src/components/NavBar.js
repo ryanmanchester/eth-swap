e2168738f683c068338dfff1b5e404a75fc985e9
Ryan Manchester <ryanm17@gmail.com>
2022-01-03 00:24:12 -0500

    begins React frontend and connects MetaMask to the ETH Swap

---
+++ src/components/NavBar.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import Identicon from 'identicon.js';
+
+const NavBar = ({account}) => {
+  return(
+    <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+      <a
+        className="navbar-brand col-sm-3 col-md-2 mr-0"
+        href="http://www.dappuniversity.com/bootcamp"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        ETH Swap
+          </a>
+        <ul className="navbar-nav px-3 ms-auto">
+          <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
+          <small className="text-secondary">
+          <small id="account">{account}</small>
+          </small>
+          </li>
+          {account ? <img classname="ml-2"
+                          width="30"
+                          height="30"
+                          src={`data:image/png;base64, ${new Identicon(account, 30).toString()}`} />
+                        : <span /> }
+        </ul>
+    </nav>
+  )
+}
+
+export default NavBar
